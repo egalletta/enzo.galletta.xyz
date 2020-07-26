ac58341ce1489fef971fc54ffa0467d165b7aa8e
Enzo E. Galletta <enzo@galletta.xyz>
2020-07-26 16:29:18 -0400

    initial commit, under construction

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import json
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/experience')
+def experience():
+    with open("jobs.json",'r') as file:
+        jobs_obj = json.load(file)
+    return render_template('experience.html', jobs=jobs_obj)
+
+@app.route('/projects')
+def projects():
+    with open("projects.json",'r') as file:
+        projs_obj = json.load(file)
+    return render_template('projects.html', projects=projs_obj)
+
+@app.route('/academic')
+def academic():
+    return render_template('academic.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
